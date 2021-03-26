b7ca1a31c10f97bd86d0d624273cb9ff05de91a7
Martin Graive <martin.graive@eleves.enpc.fr>
2021-03-26 13:59:27 +0100

    Clean code

--- kernel_methods.py
+++ kernel_methods.py
@@ -1,28 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 15 16:41:42 2021
-
-@author: quentin
-"""
-
 import numpy as np
-import pandas as pd
-import scipy.optimize as scopt
-from numba import jit
 from kernels import spectrum_kernel
-from tqdm import tqdm
 from sklearn.model_selection import StratifiedKFold
-import scipy.linalg
 import scipy.sparse as sparse
 
 from qpsolvers import solve_qp
 from cvxopt import matrix, spmatrix, solvers
 
-#%% kernel ridge regression
 
+#%% kernel ridge regression
 class KRR:
-    def __init__(self,C=1,kernel=spectrum_kernel(k=5)):
+    def __init__(self, C=1, kernel=spectrum_kernel(k=5)):
         """
         Parameters
         ----------
@@ -30,8 +17,7 @@ class KRR:
             regularization constant. The default is 1.
         Returns
         -------
-        None.
-
+        None
         """
         self.C=C
         self.alpha=0
@@ -39,10 +25,9 @@ class KRR:
         self.w=0
         self.kernel=kernel
         
-    #@jit(nopython=True)
-    def fit(self,X,Y):    
+    def fit(self, X, Y):    
         """
-        fits the KRR
+        Fits the KRR
         
         Parameters
         ----------
@@ -53,8 +38,7 @@ class KRR:
 
         Returns
         -------
-        None.
-
+        None
         """
         n = X.shape[0]
         
@@ -62,23 +46,18 @@ class KRR:
         self.phi_train = phi
         
         d = phi.shape[1]
-        #print(n,d)
-        #K = phi.dot(phi.T)
         
-        if n<=d: # to do 
-            #self.w = phi.T.dot(sparse.linalg.inv(phi.dot(phi.T)+self.C*sparse.eye(Y.shape[0]))).dot(Y)
+        if n<=d: 
             self.alpha = sparse.csr_matrix(np.linalg.inv(phi.dot(phi.T)+self.C*np.eye(Y.shape[0])).dot(Y).T)
             self.alpha = self.alpha.multiply(np.abs(self.alpha)>10e-8)
             self.w=None
-            #self.w = np.array(phi.T.dot(np.linalg.inv(phi.dot(phi.T)+self.C*np.eye(Y.shape[0])).dot(Y).T)).squeeze()
         else:
-            #self.w = sparse.linalg.inv(phi.T.dot(phi)+self.C*sparse.eye(phi.shape[0],dtype=np.int32)).dot(phi.T).dot(Y)
             self.w = np.array(np.linalg.inv(phi.T.dot(phi)+self.C*np.eye(phi.shape[0],dtype=np.int)).dot(phi.T).dot(Y)).squeeze()
         return
     
-    def predict(self,x):
+    def predict(self, x):
         """
-        predict value from input and training set
+        Predicts value from input and training set
 
         Parameters
         ----------
@@ -88,187 +67,59 @@ class KRR:
         Returns
         -------
         out : float
-            prediction.
-
+            prediction
         """
         if self.w != None:
             phi = self.kernel.phi(x)
             out = np.array(phi.dot(self.w.T).T)
-        else :
+        else:
             phi = self.kernel.phi(x)
             out = phi.dot(self.phi_train.T.dot(self.alpha))
             out = out.toarray().squeeze()
         return out
-    
-#%% kernel logistic regression ## DOES NOT WORK
 
-class KLR:
-    def __init__(self,C=1,kernel=spectrum_kernel(k=5)):
-        """
-        Parameters
-        ----------
-        C : float, optional
-            regularization constant. The default is 1.
-        Returns
-        -------
-        None.
-
-        """
-        self.C=C
-        self.w=0
-        self.kernel=kernel
-        
-    #@jit(nopython=True)
-    def fit(self,X,Y,it=10):    
-        """
-        fits the KRR
-        
-        Parameters
-        ----------
-        X : 1-D array of strings
-            ATCG inputs
-        Y : 1-D array
-            labels.
-        it : int
-            number of iterations
-
-        Returns
-        -------
-        None.
-
-        """
-        n = X.shape[0]
-        alpha = np.zeros(n) #np.random.randn(n)
-                
-        phi = self.kernel.phi(X)
-        
-        d = phi.shape[1]
-        K = phi.dot(phi.T)
-        
-        for i in tqdm(range(it)):
-            #print(i)
-            m = K.dot(alpha)
-            print("m :",m.min(),m.max())
-            #print("m:",m.shape)
-            P = -1/(1+np.exp(Y*m))
-            print("P :",P.min(),P.max())
-            #print("P :",P.shape)
-            W = 1/(1+np.exp(Y*m))/(1+np.exp(-Y*m))
-            print("W :",W.min(),W.max())
-            #print("W :",W.shape)
-            z = m + Y*(1+np.exp(-Y*m))
-            print("z :",z.min(),z.max())
-            #print("z :",z.shape)
-            
-            alpha = np.diag(np.sqrt(W)).dot( np.linalg.inv( np.diag(np.sqrt(W)).dot(K.dot(np.diag(np.sqrt(W))))+self.C*np.eye(z.shape[0]) ).dot( np.diag(np.sqrt(W)).dot(z) ) )
-            #print("alpha :",alpha.shape)
-            
-        self.w = alpha.dot(phi)
-        #print("w :",self.w.shape)
-        return
-    
-    def predict(self,x):
-        """
-        predict value from input and training set
-
-        Parameters
-        ----------
-        x : string
-            input.
-
-        Returns
-        -------
-        out : float
-            prediction.
-
-        """
-        phi = self.kernel.phi(x)
-        out = self.w.dot(phi.T)
-        return out
-    
     
 #%% SVM
-
 class SVM:
-    def __init__(self,C=1,kernel=spectrum_kernel(k=5)):
+    def __init__(self, C=1, kernel=spectrum_kernel(k=5)):
         """
         Parameters
         ----------
         C : float, optional
             regularization constant. The default is 1.
-        Returns
-        -------
-        None.
-
         """
         self.C=C
         self.alpha=0
         self.phi_train=0
         self.kernel=kernel
         
-    def fit(self,X,Y):    
-        """
-        fits the KRR
-        
-        Parameters
-        ----------
-        X : 1-D array of strings
-            ATCG inputs
-        Y : 1-D array
-            labels.
-        it : int
-            number of iterations
-
-        Returns
-        -------
-        None.
-
-        """
+    def fit(self, X, Y):
         n = X.shape[0]
-                
+        
         phi = self.kernel.phi(X)
         self.phi_train = phi
         
         K = phi.dot(phi.T).toarray()
-        
-        #alpha = solve_qp(P=2*K.astype(np.double), q=-2*Y.astype(np.double), lb=np.zeros(n,dtype=np.double), G=np.diag(Y).astype(np.double), h=self.C*np.ones(n,dtype=np.double))
         
         P = matrix(2*K.astype(np.float))
         q = matrix(-2*Y.astype(np.float))
         G = matrix(np.concatenate((np.diag(Y),-np.diag(Y))).astype(np.float))
         h = matrix(np.concatenate((self.C*np.ones(n),np.zeros(n))))       
         
-        #print("solving QP problem")
-        
         self.alpha = sparse.csr_matrix(solvers.qp(P=P, q=q, G=G, h=h, show_progress=True)['x'])
-        self.alpha = self.alpha.multiply(np.abs(self.alpha)>(self.C*10e-6))
-                        
+        self.alpha = self.alpha.multiply(np.abs(self.alpha)>(self.C*1e-6))
         return
     
-    def predict(self,x):
-        """
-        predict value from input and training set
-
-        Parameters
-        ----------
-        x : string
-            input.
-
-        Returns
-        -------
-        out : float
-            prediction.
-
-        """
+    def predict(self, x):
         phi = self.kernel.phi(x)
         out = phi.dot(self.phi_train.T.dot(self.alpha))
         out = out.toarray().squeeze()
         return out
     
+    
 #%% cross validation
-
 class cross_clf:
-    def __init__(self,C=1,kernel=spectrum_kernel(k=5),clf=SVM,n_splits=4,random_state=42):
+    def __init__(self, C=1, kernel=spectrum_kernel(k=5), clf=SVM, n_splits=4, random_state=42):
         """
         Parameters
         ----------
@@ -276,10 +127,6 @@ class cross_clf:
             regularization constant. The default is 1.
         n_splits : int, optional
             number of cross validation folds. The default is 3.
-        Returns
-        -------
-        None.
-
         """
         self.C=C
         self.kernel=kernel
@@ -287,26 +134,9 @@ class cross_clf:
         self.n_splits=n_splits
         self.random_state=random_state
         
-    def fit(self,X,Y):    
-        """
-        fits the KRR
-        
-        Parameters
-        ----------
-        X : 1-D array of strings
-            ATCG inputs
-        Y : 1-D array
-            labels.
-        it : int
-            number of iterations
-
-        Returns
-        -------
-        None.
-
-        """
+    def fit(self, X, Y):
         skf = StratifiedKFold(n_splits=self.n_splits, random_state=0, shuffle=True)
-                
+        
         i=0
         for train_index, test_index in skf.split(X, Y):  
             self.clfs[i].fit(X[train_index], Y[train_index])
@@ -314,21 +144,7 @@ class cross_clf:
             
         return
     
-    def predict(self,x):
-        """
-        predict value from input and training set
-
-        Parameters
-        ----------
-        x : string
-            input.
-
-        Returns
-        -------
-        out : float
-            prediction.
-
-        """
+    def predict(self, x):
         out = self.clfs[0].predict(x)
         for i in range(1,self.n_splits):
             out+=self.clfs[i].predict(x)
--- kernels.py
+++ kernels.py
@@ -1,106 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 15 16:03:06 2021
-
-@author: quentin
-"""
-
-#%% importations
-
 import numpy as np
-import pandas as pd
-from numba import jit 
 from itertools import product, combinations
 from scipy.special import comb
 import scipy.sparse as sparse
 from tqdm import tqdm
 
+
 #%% spectrum kernel
-
-# @jit(nopython=True)
-# def phi_spectrum(X,k):
-#     """
-#     Parameters
-#     ----------
-#     X : string
-#     ATCG string
-#     k : int, optional
-#     length of consecutive features considered. The default is 5.
-
-#     Returns
-#     -------
-#     out : array of int
-#     spectrum kernel value  
-#     """
-#     values = {'A':0,'T':1,'C':2,'G':3}
-    
-#     # if type(X)==str:
-    
-#     #     out = np.zeros(4**k).astype(np.int32)
-        
-#     #     for i in range(len(X)-k+1):
-#     #         u = X[i:i+k]
-#     #         ind = values[u[0]]
-#     #         for j in range(1,k):
-#     #             ind += values[u[j]]*4**j
-#     #         out[ind]+=1
-                
-    
-#     out = np.zeros((len(X),4**k)).astype(np.int32)
-#     for l in range(len(X)):
-#         for i in range(len(X[0])-k+1):
-#             u = X[l][i:i+k]
-#             ind = values[u[0]]
-#             for j in range(1,k):
-#                 ind += values[u[j]]*4**j
-#             out[l,ind]+=1         
-#     return out
-
-# @jit
-# def _update(X: str, k: int, values: dict[str,int]) -> tuple[np.ndarray, np.ndarray]:
-#     col_ind = np.zeros(len(X)-k+1)
-    
-#     for i in range(len(X)-k+1):
-#         u = X[i:i+k]
-#         idx = values[u[0]]
-#         for j in range(1,k):
-#             idx += values[u[j]] * 4**j
-            
-#         col_ind[i] = idx
-    
-#     col_ind, data = np.unique(col_ind, return_counts=True)  #fixme: return_counts not implemented
-    
-#     return data, col_ind
-
 class spectrum_kernel:
-    def __init__(self,k=5):
+    def __init__(self, k=5):
         self.k = k
         self.values = {'A':0,'T':1,'C':2,'G':3}
-        
-    #@jit(nopython=True)
-    def phi(self,X):
-        """
-        Parameters
-        ----------
-        X : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
     
-        Returns
-        -------
-        out : array of int
-              spectrum kernel value  
-        """
+    def phi(self, X):
         values = {'A':0,'T':1,'C':2,'G':3}
         
         if type(X)==str:
-        
-            #out = np.zeros(4**self.k).astype(np.int32)
-            #out = sparse.csr_matrix((1,4**self.k),dtype=np.int32)
             out = sparse.lil_matrix((1,4**self.k),dtype=np.int32)
-            
             
             for i in range(len(X)-self.k+1):
                 u = X[i:i+self.k]
@@ -110,8 +25,6 @@ class spectrum_kernel:
                 out[ind]+=1
                 
         else:
-            #out = np.zeros((len(X),4**self.k)).astype(np.int32)
-            #out = sparse.csr_matrix((len(X),4**self.k),dtype=np.int32)
             out = sparse.lil_matrix((len(X),4**self.k),dtype=np.int32)
             for l in tqdm(range(len(X))):
                 for i in range(len(X[0])-self.k+1):
@@ -121,52 +34,8 @@ class spectrum_kernel:
                         ind += values[u[j]]*4**j
                     out[l,ind]+=1         
         return sparse.csc_matrix(out)
-            
-    # def phi(self,X):
-    #     """
-    #     Parameters
-    #     ----------
-    #     X : string
-    #         ATCG string
-    #     k : int, optional
-    #         length of consecutive features considered. The default is 5.
-    
-    #     Returns
-    #     -------
-    #     out : array of int
-    #           spectrum kernel value  
-    #     """
-    #     if type(X)==str:
-    #         data, col_ind = _update(X, self.k, self.values)
-    #         return sparse.csc_matrix((data, (np.zeros(len(col_ind)), col_ind)), shape=(1, 4**self.k))
-                
-    #     else:
-    #         data_all, col_ind_all, row_ind_all = np.array([]), np.array([]), np.array([])
-    #         for l in range(len(X)):
-    #             data, col_ind = _update(X[l], self.k, self.values)
-    #             data_all = np.append(data_all, data)
-    #             col_ind_all = np.append(col_ind_all, col_ind)
-    #             row_ind_all = np.append(row_ind_all, np.full(len(data), l))
-    #         return sparse.csc_matrix((data_all, (row_ind_all, col_ind_all)), shape=(len(X), 4**self.k))
-
-    #@jit(nopython=True)
-    def __call__(self,X1,X2):
-        """
-        Parameters
-        ----------
-        X1 : string
-            ATCG string
-        X2 : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
-    
-        Returns
-        -------
-        out : array of int
-              spectrum kernel value  
-        """
         
+    def __call__(self, X1, X2):
         phi1 = self.phi(X1)
         phi2 = self.phi(X2)
             
@@ -179,31 +48,16 @@ class mismatch_kernel:
         self.k = k
         self.m = m
         self.values = {'A':0,'T':1,'C':2,'G':3}
-        self.tab = 4 ** np.arange(k,dtype=np.int)
+        self.tab = 4 ** np.arange(k,dtype=int)
         
-        self.mis = np.zeros((int(comb(k, m)) * 4 ** m, k),dtype=np.int)
-        shift = np.array(list(product([0,1,2,3], repeat=m)),dtype=np.int)
+        self.mis = np.zeros((int(comb(k, m)) * 4 ** m, k),dtype=int)
+        shift = np.array(list(product([0,1,2,3], repeat=m)),dtype=int)
         for i, idx in enumerate(combinations(range(k), m)):
             self.mis[i*4**m:(i+1)*4**m, idx] = shift
-        # self.mis = np.unique(self.mis, axis=0)
         
     def phi(self, X):
-        """
-        Parameters
-        ----------
-        X : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
-        m : int, optional
-            number of mismatch allowed. The default is 2.
-        Returns
-        -------
-        out : array of int
-            mismatch kernel value
-        """
         if type(X) == str:
-            out = sparse.lil_matrix((1, 4**self.k), dtype=np.int)
+            out = sparse.lil_matrix((1, 4**self.k), dtype=int)
             X = list(map(lambda x: self.values[x], list(X))) + [0] * (self.k-len(X)%self.k)
             for i in range(len(X)-self.k+1):
                 u = X[i:i+self.k]
@@ -213,19 +67,19 @@ class mismatch_kernel:
                     out[0, j] += 1
                     
         else:
-            out = sparse.lil_matrix((len(X), 4**self.k), dtype=np.int)
+            out = sparse.lil_matrix((len(X), 4**self.k), dtype=int)
             for l in tqdm(range(len(X))):
                 XX = list(map(lambda x: self.values[x], list(X[l]))) + [0] * (self.k-len(X[l])%self.k)
                 for i in range(len(X[l])-self.k+1):
                     u = XX[i:i+self.k]
                     ind = ((u + self.mis) % 4) * self.tab
-                    ind = ind.sum(1)#.astype(np.int)
+                    ind = ind.sum(1)
                     for j in ind:
                         out[l, j] += 1
                         
         return out.tocsc()
         
-    def __call__(self,X1,X2):
+    def __call__(self, X1, X2):
         phi1 = self.phi(X1)
         phi2 = self.phi(X2)
         return phi1.dot(phi2.T)
@@ -233,104 +87,19 @@ class mismatch_kernel:
 #%% Concatenation
 
 class concat_kernel:
-    def __init__(self,kernels=[spectrum_kernel(k=5),spectrum_kernel(k=10)],weights=[1,1]):
+    def __init__(self, kernels=[spectrum_kernel(k=5), spectrum_kernel(k=10)], weights=[1, 1]):
         self.kernels=kernels
         self.weights=weights
         
-    def phi(self,X):
-        """
-        Parameters
-        ----------
-        X : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
-    
-        Returns
-        -------
-        out : array of int
-              spectrum kernel value  
-        """
-        out=self.kernels[0].phi(X)*self.weights[0]
+    def phi(self, X):
+        out = self.kernels[0].phi(X)*self.weights[0]
         
-        for i in range(1,len(self.kernels)):
-            out = sparse.hstack([out,self.kernels[i].phi(X)*self.weights[i]])
+        for i in range(1, len(self.kernels)):
+            out = sparse.hstack([out, self.kernels[i].phi(X)*self.weights[i]])
         
         return out
     
-    def __call__(self,X1,X2):
+    def __call__(self, X1, X2):
         phi1 = self.phi(X1)
         phi2 = self.phi(X2)
         return phi1.dot(phi2.T)
-
-
-#%% substring kernel
-
-class substring_kernel:
-    def __init__(self,k=5,lbd=0.5):
-        self.k=k
-        self.lbd=lbd
-        
-    #@jit(nopython=True)
-    def phi(self,X):
-        """
-        Parameters
-        ----------
-        X : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
-        lbd : float, optional
-            decay when gaps. Must be between 0 and 1 The default is 0.5
-    
-        Returns
-        -------
-        out : array of int
-              spectrum kernel value  
-        """
-        values = {'A':0,'T':1,'C':2,'G':3}
-        
-        if type(X)==str:
-        
-            out = np.zeros(4**self.k).astype(np.int32)
-            
-            for i in range(len(X)-self.k+1):
-                u = X[i:i+self.k]
-                ind = values[u[0]]
-                for j in range(1,self.k):
-                    ind += values[u[j]]*4**j
-                out[ind]+=1
-                
-        else:
-            out = np.zeros((len(X),4**self.k)).astype(np.int32)
-            for l in range(len(X)):
-                for i in range(len(X[0])-self.k+1):
-                    u = X[l][i:i+self.k]
-                    ind = values[u[0]]
-                    for j in range(1,self.k):
-                        ind += values[u[j]]*4**j
-                    out[l,ind]+=1         
-        return out
-
-    #@jit(nopython=True)
-    def __call__(self,X1,X2):
-        """
-        Parameters
-        ----------
-        X1 : string
-            ATCG string
-        X2 : string
-            ATCG string
-        k : int, optional
-            length of consecutive features considered. The default is 5.
-    
-        Returns
-        -------
-        out : array of int
-              spectrum kernel value  
-        """
-        
-        phi1 = self.phi(X1).astype(np.int32)
-        phi2 = self.phi(X2).astype(np.int32)
-            
-        return np.dot(phi1,phi2.T)
